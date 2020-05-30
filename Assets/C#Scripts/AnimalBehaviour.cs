22823508eab6688f514a58556c9082f216255229
Иван Евсюков <evsyukov98@list.ru>
2020-05-31 01:52:06 +0700

    Создано поведение ботов, Генерация ботов.

---
+++ Assets/C#Scripts/AnimalBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimalBehaviour : MonoBehaviour
+{
+    public float Speed;
+    private float waitTime;
+    public float StartWaitTime;
+
+    // список точек куда будет двигаться обьект
+    public GameObject[] moveSpots;
+
+    // Случайная цифра для рандомизации к точке движения
+    private int randomSpot;
+
+    private void Awake()
+    {
+        moveSpots = GameObject.FindGameObjectsWithTag("Bush");
+    }
+    void Start()
+    {
+        waitTime = StartWaitTime;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(
+            transform.position,
+            moveSpots[randomSpot].transform.position, 
+            Speed* Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[randomSpot].transform.position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = StartWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    
+}
---
+++ Assets/C#Scripts/Game_1/GameManager_1.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_1 : MonoBehaviour
+{
+    // счетчик
+    private int _goal;
+
+    // животное которое будет генериться
+    public List<GameObject> AnimalPrefabs = new List<GameObject>();
+
+    // Список позиций для спауна животных.
+    public List<Transform> SpawnPoints = new List<Transform>();
+
+    void Start()
+    {
+        Guide();
+
+        CreateAnimals();
+
+    }
+
+    void Guide()
+    {
+        // Здесь будет обучение игры
+    }
+
+    //TODO: Нужно добавить flip
+    void CreateAnimals()
+    {
+        int randomAnimal = Random.Range(0, AnimalPrefabs.Count);
+
+        Transform spawnPoint = SpawnPoints[Random.Range(0,SpawnPoints.Count)];
+
+        Instantiate(AnimalPrefabs[randomAnimal],spawnPoint);
+    }
+
+    void GoalOfVictory()
+    {
+        // отслеживание прогресса игры
+        // после 5, 10, 20 нажатий на цель
+        // будет заканчиваться игра
+    }
+}
